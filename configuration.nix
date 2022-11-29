da3f7341ebe4bec162d4b1ac84642a3cf4a7133c
David Whiting <david@davidwhiting.me.uk>
2022-11-29 09:20:37 +0000

    Added basic configuration.nix and flake.nix

---
+++ configuration.nix
@@ -0,0 +1,32 @@
+{ modulesPath, lib, pkgs, ... }:
+{
+  imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
+    (modulesPath + "/virtualisation/digital-ocean-config.nix")
+  ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.david = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    tmux
+    killall
+  ];
+
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+}
